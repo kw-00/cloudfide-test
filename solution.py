5f25ee20713545bc272635960e8fea4418756e84
kw-00 <christopher.p.wojcik@gmail.com>
2026-03-20 22:33:30 +0100

    Refactor function hierarchy

--- solution.py
+++ solution.py
@@ -3,19 +3,15 @@ from __future__ import annotations
 import pandas as pd
 import re
 import logging
-from typing import Literal, List
-
-import re
-
+from typing import List
 
 _COLUMN_PATTERN = r"[^\W\d]+"
 _OPERATOR_PATTERN = r"[\+\-\*]"
 _ALLOWED_WHITESPACES = r"\s"
 
 _column_pattern_compiled = re.compile(_COLUMN_PATTERN)
-_operator_pattern_compiled = re.compile(_OPERATOR_PATTERN)
-_full_pattern = re.compile(rf"({_COLUMN_PATTERN}|{_OPERATOR_PATTERN}|{_ALLOWED_WHITESPACES})")
-
+_full_pattern = re.compile(rf"{_COLUMN_PATTERN}|{_OPERATOR_PATTERN}|{_ALLOWED_WHITESPACES}")
+_no_whitespaces_pattern = re.compile(rf"({_COLUMN_PATTERN}|{_OPERATOR_PATTERN})")
 
 def add_virtual_column(df: pd.DataFrame, role: str, new_column: str) -> pd.DataFrame:
     try:
@@ -30,19 +26,33 @@ def add_virtual_column(df: pd.DataFrame, role: str, new_column: str) -> pd.DataF
 
 def _get_virtual_column(df: pd.DataFrame, role: str) -> pd.Series:
     tokens = _tokenize_role(role)
+    _verify_token_sequence(tokens)
+    return df.eval("".join(tokens)) # type: ignore
 
-    # A new list of tokens, with column names substituted 
-    # with DataFrame queries and whitespace tokens removed.
 
-    # Example: x * y + z ===> df["x"] * df["y"] + df["z"]
-    transformed_tokens = [] 
+def _tokenize_role(role: str) -> List[str]:
+    if _role_has_invalid_characters(role):
+        raise RoleFormatError("Parameter \"role\" contains invalid characters.")
 
+    tokens = _no_whitespaces_pattern.findall(role)
+    return tokens
+
+
+def _role_has_invalid_characters(role: str) -> bool:
+    tokens = _full_pattern.findall(role)
+    characters_matched = "".join(tokens)
+    has_invalid_characters = len(characters_matched) != len(role) 
+    return has_invalid_characters
+
+
+def _verify_token_sequence(tokens: List[str]):
     def get_role_with_token_highlighted(token_index: int):
         idx = token_index
-        return "".join(tokens[:idx]) + f">>>{tokens[idx]}<<<" + "".join(tokens[idx + 1:])
+        before = "".join(tokens[:idx])
+        after = "".join(tokens[idx + 1:])
+        return before + f">>>{tokens[idx]}<<<" + after
 
     column_expected = True
-    operator_expected = False
     for idx, token in enumerate(tokens):
         if _is_column(token):
             if not column_expected:
@@ -54,12 +64,11 @@ def _get_virtual_column(df: pd.DataFrame, role: str) -> pd.Series:
                     + "\n\tcol_& + col_2"
                     + f"\n\nProblematic part:\n\t{get_role_with_token_highlighted(idx)}"
                 )
-            transformed_tokens.append(f"df[\"{token}\"]")
             column_expected = False
             operator_expected = True
-
-        elif _is_operator(token):
-            if not operator_expected:
+        # If token is not column, it must be operator
+        else:
+            if column_expected:
                 raise RoleFormatError(
                     "Parameter \"role\" value is not valid. Expression may not start with an operator,"
                     + " nor can it contain multiple operators in a row. "
@@ -68,33 +77,12 @@ def _get_virtual_column(df: pd.DataFrame, role: str) -> pd.Series:
                     + "\n\tcol_1 + + col_2"
                     + f"\n\nProblematic part:\n\t{get_role_with_token_highlighted(idx)}"
                 )
-            transformed_tokens.append(token)
             column_expected = True
-            operator_expected = False
-
-    # Join transformed tokens together into python code and evaluate to get resulting pd.Series
-    return eval("".join(transformed_tokens))
-
-
-def _tokenize_role(role: str) -> List[str]:
-    tokens = _full_pattern.findall(role)
-
-    characters_matched = "".join(tokens)
-    has_invalid_characters = len(characters_matched) != len(role) 
-    if has_invalid_characters:
-        raise RoleFormatError("Parameter \"role\" contains invalid characters.")
-    return tokens
 
 
 def _is_column(token: str) -> bool:
     return _column_pattern_compiled.fullmatch(token) != None
 
 
-def _is_operator(token: str) -> bool:
-    return _operator_pattern_compiled.fullmatch(token) != None
-
-
 class RoleFormatError(RuntimeError):
     pass
-
-
